088029b9b16b1993b33849222f006d41c1416f59
William Gautier <william.gautier@hpe.com>
2016-10-27 15:57:15 -0700

    Clarify expected output of tests

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -13,11 +13,12 @@ import (
 	"github.com/hpcloud/testbrain/lib"
 )
 
-const defaultTimeout = 300 * time.Second
-
 var (
+	defaultTimeout = 300 * time.Second
 	defaultInclude = "_test\\.sh$"
 	defaultExclude = "^$"
+
+	redBoldString = color.New(color.FgRed, color.Bold).SprintfFunc()
 )
 
 func setupTestUI() (*bytes.Buffer, *bytes.Buffer) {
@@ -209,7 +210,10 @@ func TestRunSingleTestTimeout(t *testing.T) {
 
 	testFolder, _ := filepath.Abs("../testdata")
 	testResult := runSingleTest("timeout_test.sh", testFolder, 1*time.Second)
-	expectedOutput := "Stuck in an infinite loop!\nStuck in an infinite loop!\nStuck in an infinite loop!\nKilled by testbrain: Timed out after 1s"
+	expectedOutput := "Stuck in an infinite loop!\n" +
+		"Stuck in an infinite loop!\n" +
+		"Stuck in an infinite loop!\n" +
+		"Killed by testbrain: Timed out after 1s"
 	expected := lib.TestResult{
 		TestFile: "timeout_test.sh",
 		Success:  false,
@@ -224,8 +228,10 @@ func TestRunSingleTestTimeout(t *testing.T) {
 func TestOutputResults(t *testing.T) {
 	_, out := setupTestUI()
 	failedTestResults := setupFailedTestResults()
-	outputResults(failedTestResults, 5)
-	expected := "\x1b[31;1mtestfile1: Failed with exit code 1\n\x1b[0m\x1b[31;1mtestfile2: Failed with exit code 2\n\x1b[0m\x1b[31;1m\nTests complete: 3 Passed, 2 Failed\n\x1b[0m"
+	outputResults(failedTestResults, 5, 42)
+	expected := redBoldString("testfile1: Failed with exit code 1\n") +
+		redBoldString("testfile2: Failed with exit code 2\n") +
+		redBoldString("\nTests complete: 3 Passed, 2 Failed\n")
 	if got := out.String(); got != expected {
 		t.Fatalf("Expected:\n %q\n\nHave:\n %q\n", expected, got)
 	}
@@ -280,7 +286,7 @@ func TestPrintVerboseSingleTestResult(t *testing.T) {
 func TestOutputResultsJSON(t *testing.T) {
 	_, out := setupTestUI()
 	failedTestResults := setupFailedTestResults()
-	outputResultsJSON(failedTestResults, 5)
+	outputResultsJSON(failedTestResults, 5, 42)
 	expected := `{"passed":3,"failed":2,"failedList":[{"filename":"testfile1","success":false,"exitcode":1,"output":"It didn't work!"},{"filename":"testfile2","success":false,"exitcode":2,"output":"It didn't work again!"}]}`
 	if got := out.String(); got != expected {
 		t.Fatalf("Expected:\n %q\n\nHave:\n %q\n", expected, got)
@@ -289,7 +295,7 @@ func TestOutputResultsJSON(t *testing.T) {
 
 func TestRunCommandSuccess(t *testing.T) {
 	testFolder, _ := filepath.Abs("../testdata/success")
-	err := runCommand([]string{testFolder}, defaultInclude, defaultExclude, defaultTimeout, true, false, false)
+	err := runCommand([]string{testFolder}, defaultInclude, defaultExclude, defaultTimeout, true, false, false, defaultSeed)
 	if err != nil {
 		t.Fatalf("Didn't expect an error, got '%s'", err)
 	}
@@ -297,7 +303,7 @@ func TestRunCommandSuccess(t *testing.T) {
 
 func TestRunCommandFailure(t *testing.T) {
 	testFolder, _ := filepath.Abs("../testdata/failure")
-	err := runCommand([]string{testFolder}, defaultInclude, defaultExclude, defaultTimeout, true, false, false)
+	err := runCommand([]string{testFolder}, defaultInclude, defaultExclude, defaultTimeout, true, false, false, defaultSeed)
 	if err == nil {
 		t.Fatal("Expected to get an error, got 'nil'")
 	}
