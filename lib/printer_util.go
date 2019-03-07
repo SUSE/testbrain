30dd577707f7a5a3112a2c23e866fc8f9e46fbcd
Thulio Ferraz Assis <tassis@suse.com>
2019-03-07 14:04:19 -0800

    Missing punctuation

--- lib/printer_util.go
+++ lib/printer_util.go
@@ -5,12 +5,12 @@ import (
 )
 
 var (
-	// Green is a convenient color helper
+	// Green is a convenient color helper.
 	Green = color.New(color.FgGreen).SprintfFunc()
-	// GreenBold is a convenient color helper
+	// GreenBold is a convenient color helper.
 	GreenBold = color.New(color.FgGreen, color.Bold).SprintfFunc()
-	// Red is a convenient color helper
+	// Red is a convenient color helper.
 	Red = color.New(color.FgRed).SprintfFunc()
-	// RedBold is a convenient color helper
+	// RedBold is a convenient color helper.
 	RedBold = color.New(color.FgRed, color.Bold).SprintfFunc()
 )
