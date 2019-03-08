3a0013d064dd08073eefab39ca592a82e3b99331
Thulio Ferraz Assis <tassis@suse.com>
2019-03-07 16:38:20 -0800

    Move color variables to runner.go

--- lib/runner.go
+++ lib/runner.go
@@ -13,10 +13,23 @@ import (
 	"sort"
 	"syscall"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 const (
 	unknownExitCode = -1
+)
+
+var (
+	// Green is a convenient color helper.
+	Green = color.New(color.FgGreen).SprintfFunc()
+	// GreenBold is a convenient color helper.
+	GreenBold = color.New(color.FgGreen, color.Bold).SprintfFunc()
+	// Red is a convenient color helper.
+	Red = color.New(color.FgRed).SprintfFunc()
+	// RedBold is a convenient color helper.
+	RedBold = color.New(color.FgRed, color.Bold).SprintfFunc()
 )
 
 // RunnerOptions represents options passed to the Runner.
